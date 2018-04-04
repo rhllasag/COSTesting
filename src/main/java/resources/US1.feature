47d413125d2c254237b5b837912f7473bc22a772
homer <homer_llasag1@yahoo.com>
2018-04-04 15:41:40 +0100

    Initial commit

---
+++ src/main/java/resources/US1.feature
@@ -0,0 +1,8 @@
+Feature: Access to the Contacts Orchestrator Solution's (COS) Landing Page
+  As a user
+  I want to access to the URL address provided for the landing page
+  So that I can see the list of the contacts available
+  Scenario: Landing page's title and text contains "Contacts Orchestrator Solution"
+    Given I access the landing page of COS
+    Then the title of the page should be "Contacts Orchestrator Solution"
+    And I can see the text "Contacts Orchestrator Solution"
---
+++ src/test/java/US1StepsDef.java
@@ -0,0 +1,41 @@
+import cucumber.api.PendingException;
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.And;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import static junit.framework.TestCase.assertEquals;
+
+public class US1StepsDef {
+    private WebDriver driver;
+
+    @Given("^I access the landing page of COS$")
+    public void iAccessTheLandingPageOfCOS() throws Throwable {
+        driver.get("http://localhost:8080");
+        assertEquals ("Contacts Orchestrator Solution",driver.getTitle());
+
+    }
+
+    @Then("^the title of the page should be \"([^\"]*)\"$")
+    public void theTitleOfThePageShouldBe(String title) throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.titleContains(title));
+    }
+
+    @And("^I can see the text \"([^\"]*)\"$")
+    public void iCanSeeTheText(String title) throws Throwable {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.textToBePresentInElement(
+                driver.findElement(By.tagName("body")),text));
+    }
+    @Before
+    public void setUp()throws Exception{
+        driver = new HtmlUnitDriver();
+    }
+    @After
+    public void tearDown()throws Exception{
+
+    }
+}
